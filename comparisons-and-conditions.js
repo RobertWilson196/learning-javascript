0c1a1f17b7c5a258e743ad4e9e24b186764ed24f
Robert Wilson <youmufan@gmail.com>
2018-01-29 21:00:41 -0500

    added comparisons and conditions

---
+++ comparisons-and-conditions.js
@@ -0,0 +1,27 @@
+// Comparison Operators
+// <, <=, ===, >=, >, !==
+
+console.log();
+console.log("Comparisons and Conditions");
+console.log();
+
+const aString = "abc";
+const anotherString = "abcd";
+const aNumber = 123;
+const anotherNumber = 123.2;
+
+console.log("aString < anotherString: ", aString < anotherString);      // True
+console.log("aString <= anotherString: ", aString <= anotherString);    // True
+console.log("aString === anotherString: ", aString === anotherString);  // False
+console.log("aString >= anotherString: ", aString >= anotherString);    // False
+console.log("aString > anotherString: ", aString > anotherString);      // False
+console.log("aString !== anotherString: ", aString !== anotherString);  // True
+console.log();
+console.log("aNumber < anotherNumber: ", aNumber < anotherNumber);      // True
+console.log("aNumber <= anotherNumber: ", aNumber <= anotherNumber);    // True
+console.log("aNumber === anotherNumber: ", aNumber === anotherNumber);  // False
+console.log("aNumber >= anotherNumber: ", aNumber >= anotherNumber);    // False
+console.log("aNumber > anotherNumber: ", aNumber > anotherNumber);      // False
+console.log("aNumber !== anotherNumber: ", aNumber !== anotherNumber);  // True
+console.log();
+
