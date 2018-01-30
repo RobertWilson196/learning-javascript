b2b08c1468265390292eff2213f8a1e2c47da0e0
Robert Wilson <youmufan@gmail.com>
2018-01-29 19:35:37 -0500

    explore the basic creation of strings and numbers

--- variables-and-datatypes.js
+++ variables-and-datatypes.js
@@ -3,11 +3,31 @@
  *  Multi-Line Comment 
  */
 
- // Variables
- var name = "Robert";       // Old School JavaScript (Don't Use This)
- const fruit = "Banana";    // Constant Binding (locked)
- let veggie = "Carrot";     // Similar to var, but with differences*
+// Variables
+var name = "Robert";       // Old School JavaScript (Don't Use This)
+const fruit = "Banana";    // Constant Binding (locked)
+let veggie = "Carrot";     // Similar to var, but with differences*
 
- console.log("Name: ", name);       // Print
- console.log("Fruit: ", fruit);
- console.log("Veggie: ", veggie);
+console.log("Name: ", name);       // Print
+console.log("Fruit: ", fruit);
+console.log("Veggie: ", veggie);
+
+console.log(".~-^-~.~-^-~.~-^-~.~-^-~.~-^-~.");
+// Simple Data Types
+ // Strings
+const aString = "things and stuff";     // Camel Casing (can use single quotes)
+const anotherString = "more stuff, less things";
+const numericString = "123abc";
+console.log("aString: ", aString);
+console.log("anotherString: ", anotherString);
+console.log("numericString: ", numericString);
+console.log("What is the type of aString?: ", typeof aString);
+
+console.log(".~-^-~.~-^-~.~-^-~.~-^-~.~-^-~.");
+ // Numbers
+const aNumber = 1;
+const anotherNumber = 3.5;
+console.log(aNumber, anotherNumber);
+console.log("What is the type of aNumber?: ", typeof aNumber);
+console.log("What is the type of anotherNumber?: ", typeof anotherNumber);
+
